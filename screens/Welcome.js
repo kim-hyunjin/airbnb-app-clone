d73c089c98e58b1f80c4886ef88f453cfd67ee91
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-09 22:28:04 +0900

    웰컴화면 버튼

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,8 +1,8 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
 import styled from "styled-components/native";
 import { StatusBar } from "react-native";
 import { BlurView } from "expo-blur";
+import Btn from "../components/Auth/Btn";
 
 const LOGO_URL =
   "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png";
@@ -22,21 +22,31 @@ const Logo = styled.Image`
   height: 100px;
 `;
 
-export default ({ navigation }) => (
-  <Container>
-    <BlurView
-      intensity={40}
-      tint="light"
-      style={{
-        flex: 1,
-        width: "100%",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Logo source={{ uri: LOGO_URL }} />
-    </BlurView>
-    <Image source={require("../assets/loginBg.jpg")} />
-    <StatusBar barStyle="auto" />
-  </Container>
-);
+const BtnContainer = styled.View``;
+
+export default ({ navigation }) => {
+  const goToSignUp = () => navigation.navigate("SignUp");
+  const goToSignIn = () => navigation.navigate("SignIn");
+  return (
+    <Container>
+      <BlurView
+        intensity={40}
+        tint="light"
+        style={{
+          flex: 1,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Logo source={{ uri: LOGO_URL }} />
+        <BtnContainer>
+          <Btn onPress={goToSignUp} text={"Sign Up"} accent={true} />
+          <Btn onPress={goToSignIn} text={"Sign In"} />
+        </BtnContainer>
+      </BlurView>
+      <Image source={require("../assets/loginBg.jpg")} />
+      <StatusBar barStyle="auto" />
+    </Container>
+  );
+};
