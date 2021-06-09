8d5fd3ff658b77470f166dce377bbfea547c5154
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-09 21:39:49 +0900

    welcome screen

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -3,12 +3,9 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Welcome from "../screens/Welcome";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
-import { Platform, View } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import BackBtn from "../components/Auth/BackBtn";
 
 const Auth = createStackNavigator();
-
-const isAndroid = Platform.OS === "android";
 
 export default () => (
   <Auth.Navigator
@@ -16,17 +13,18 @@ export default () => (
     screenOptions={{
       headerBackTitleVisible: false,
       headerTransparent: true,
-      headerBackImage: () => (
-        <View style={{ paddingLeft: 20 }}>
-          <Ionicons
-            name={isAndroid ? "md-arrow-down" : "ios-arrow-down"}
-            size={28}
-          />
-        </View>
-      ),
+      headerBackImage: () => <BackBtn />,
     }}
   >
-    <Auth.Screen name="Welcome" component={Welcome} />
+    <Auth.Screen
+      name="Welcome"
+      component={Welcome}
+      options={{
+        headerTitleStyle: {
+          color: "white",
+        },
+      }}
+    />
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
   </Auth.Navigator>
