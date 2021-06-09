2aac50d285bc9f1972e19654c747d5609372111d
kim-hyunjin <hyunjin1612@gmail.com>
2021-06-09 20:46:20 +0900

    auth screen

--- navigation/Auth.js
+++ navigation/Auth.js
@@ -3,11 +3,29 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Welcome from "../screens/Welcome";
 import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
+import { Platform, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Auth = createStackNavigator();
 
+const isAndroid = Platform.OS === "android";
+
 export default () => (
-  <Auth.Navigator>
+  <Auth.Navigator
+    mode="modal"
+    screenOptions={{
+      headerBackTitleVisible: false,
+      headerTransparent: true,
+      headerBackImage: () => (
+        <View style={{ paddingLeft: 20 }}>
+          <Ionicons
+            name={isAndroid ? "md-arrow-down" : "ios-arrow-down"}
+            size={28}
+          />
+        </View>
+      ),
+    }}
+  >
     <Auth.Screen name="Welcome" component={Welcome} />
     <Auth.Screen name="SignIn" component={SignIn} />
     <Auth.Screen name="SignUp" component={SignUp} />
--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default () => {
+export default () => (
   <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
     <Text>Sign In</Text>
-  </View>;
-};
+  </View>
+);
