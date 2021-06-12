46c72917fcac29c0d23bf525e9029129fdfb8c18
Kim <hyunjin1612@gmail.com>
2021-06-12 13:17:58 +0900

    rooms slice 설정

--- redux/rootReducer.js
+++ redux/rootReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import usersReducer from "./usersSlice";
+import roomsSlice from "./roomsSlice";
 
 export default combineReducers({
   usersReducer,
+  roomsSlice,
 });
